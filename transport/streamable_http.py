4ddd841e6f0ff5925a9c6e3da794b3ff9d7454be
XSKW11111 <39919931+XSKW11111@users.noreply.github.com>
2025-10-18 14:52:36 -0700

    feat(zoom): add access_toke for sse and streamable_http

--- transport/streamable_http.py
+++ transport/streamable_http.py
@@ -3,7 +3,10 @@ from mcp.server.lowlevel import Server
 from mcp.server.streamable_http_manager import StreamableHTTPSessionManager
 from starlette.types import Scope, Receive, Send
 
-def initialise_streamable_http(app: Server, session_manager: StreamableHTTPSessionManager, logger: logging.Logger) -> callable:
+from auth import ZoomAuthenticator
+from data.base import auth_token_context
+
+def initialise_streamable_http(app: Server, session_manager: StreamableHTTPSessionManager, logger: logging.Logger, access_token: str) -> callable:
     
     async def handle_streamable_http(
         scope: Scope, receive: Receive, send: Send
@@ -11,10 +14,11 @@ def initialise_streamable_http(app: Server, session_manager: StreamableHTTPSessi
         logger.info("Handling StreamableHTTP request")
         
         # Extract auth token from headers
-        auth_token = extract_access_token(scope)
+        print(f"Access token: {access_token}")
+        logger.info(f"Access token: {access_token}")
         
         # Set the auth token in context for this request
-        token = auth_token_context.set(auth_token)
+        token = auth_token_context.set(access_token)
         try:
             await session_manager.handle_request(scope, receive, send)
         finally:
